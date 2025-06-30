3b6da7814a005a938029b5a9dd7d73f396e6f4f1
Giancarlo Tomasello <gtomasel@ucsc.edu>
2025-06-30 15:31:03 -0700

    User can edit todos now

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,6 +1,47 @@
+import { useState } from "react";
 
 function Todo(props) {
-    return(
+    const [isEditing, setEditing] = useState(false);
+    const [newName, setNewName] = useState("");
+
+    function handleChange(e){
+        setNewName(e.target.value);
+    }
+
+    function handleSubmit(e){
+        e.preventDefault();
+        props.editTask(props.id, newName);
+        setNewName("");
+        setEditing(false);
+    }
+
+    const editingTemplate = (
+        <form className="stack-small" onSubmit={handleSubmit}>
+            <div className="form-group">
+            <label className="todo-label" htmlFor={props.id}>
+                New name for {props.name}
+            </label>
+            <input 
+                id={props.id} 
+                className="todo-text" 
+                type="text" 
+                value={newName}
+                onChange={handleChange}
+            />
+            </div>
+            <div className="btn-group">
+            <button type="button" className="btn todo-cancel" onClick={() => setEditing(false)}>
+                Cancel
+                <span className="visually-hidden">renaming {props.name}</span>
+            </button>
+            <button type="submit" className="btn btn__primary todo-edit">
+                Save
+                <span className="visually-hidden">new name for {props.name}</span>
+            </button>
+            </div>
+        </form>
+    );
+    const displayTemplate = (
         <li className="todo stack-small">
             <div className="c-cb">
                 <input 
@@ -14,7 +55,7 @@ function Todo(props) {
                 </label>
             </div>
             <div className="btn-group">
-                <button type="button" className="btn">
+                <button type="button" className="btn" onClick={() => setEditing(true)}>
                 Edit <span className="visually-hidden">{props.name}</span>
                 </button>
                 <button 
@@ -26,6 +67,7 @@ function Todo(props) {
             </div>
         </li>
     );
+    return <li className="todo">{isEditing ? editingTemplate : displayTemplate}</li>;
 }
 
 export default Todo;
